6b8a952e10c7a25ab5f25e2abb0e8cf3c4dc108d
Nikita Gavshin <nikita.gavshin@piano.io>
2023-10-17 11:41:34 +0400

    fix: product file

--- app/(.)product/[id]/page.tsx
+++ app/(.)product/[id]/page.tsx
@@ -8,20 +8,23 @@ import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 function Modal() {
-  const [isOpen, setIsOpen] = useState(true);
-  const [product, setProduct] = useState<Product>();
-  const [isLoading, setIsLoading] = useState(false);
+  let [isOpen, setIsOpen] = useState(true);
   const id = useParams().id;
+  const [product, setProduct] = useState<Product>();
+  const [isLoading, setIsisLoading] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
     async function fetchProduct() {
-      setIsLoading(true);
+      setIsisLoading(true);
       const res = await fetch(`https://fakestoreapi.com/products/${id}`);
       const product = await res.json();
+
       setProduct(product);
-      setIsLoading(false);
+
+      setIsisLoading(false);
     }
+
     fetchProduct();
   }, [id]);
 
@@ -32,78 +35,77 @@ function Modal() {
         setIsOpen(false);
         router.back();
       }}
+      className="relative z-50"
     >
-      <div className="fixed inset-0 bg-black/30" aria-hidden="true">
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4">
-            <Dialog.Panel className="mx-auto max-w-3xl rounded bg-white p-10">
-              {isLoading ? (
-                <div className="h-8 w-8 rounded-full border-2 border-dotted border-blue-600 animate-spin"></div>
-              ) : (
-                <div className="flex gap-x-8 h-96">
-                  {product?.image && (
-                    <div className="relative w-72 h-full hidden md:inline">
-                      <ProductImage product={product} fill />
-                    </div>
-                  )}
-                  <div className="flex-1 flex flex-col">
-                    <div className="flex-1">
-                      <h4 className="font-semibold">{product?.title}</h4>
-                      <p className="font-medium text-sm">${product?.price}</p>
-                      <div className="flex items-center text-sm my-4">
-                        <p>{product?.rating.rate}</p>
+      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
 
-                        {product?.rating.rate && (
-                          <div className="flex items-center ml-2 mr-6">
-                            {Array.from(
-                              { length: Math.floor(product.rating.rate) },
-                              (_, i) => (
-                                <StarIcon
-                                  key={i}
-                                  className="h-4 w-4 text-yellow-500"
-                                />
-                              )
-                            )}
+      <div className="fixed inset-0 overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center p-4">
+          <Dialog.Panel className="mx-auto max-w-3xl rounded bg-white p-10">
+            {isLoading ? (
+              <div className="h-8 w-8 rounded-full border-2 border-dotted border-blue-600 animate-spin" />
+            ) : (
+              <div className="flex gap-x-8 h-96">
+                {product?.image && (
+                  <div className="relative w-72 h-full hidden md:inline">
+                    <ProductImage product={product} fill />
+                  </div>
+                )}
+                <div className="flex-1 flex flex-col">
+                  <div className="flex-1">
+                    <h4 className="font-semibold">{product?.title}</h4>
+                    <p className="font-medium text-sm">${product?.price}</p>
 
-                            {Array.from(
-                              { length: 5 - Math.floor(product.rating.rate) },
-                              (_, i) => (
-                                <StarIconOutline
-                                  key={i}
-                                  className="h-4 w-4 text-yellow-500"
-                                />
-                              )
-                            )}
-                          </div>
-                        )}
-                        <p className="text-blue-600 hover:underline cursor-pointer text-xs">
-                          See all {product?.rating.count} reviews
-                        </p>
-                      </div>
+                    <div className="flex items-center text-sm my-4">
+                      <p>{product?.rating.rate}</p>
+                      {product?.rating.rate && (
+                        <div className="flex items-center ml-2 mr-6">
+                          {Array.from(
+                            { length: Math.floor(product.rating.rate) },
+                            (_, i) => (
+                              <StarIcon
+                                key={i}
+                                className="h-4 w-4 text-yellow-500"
+                              />
+                            )
+                          )}
 
-                      <p className="line-clamp-5 text-sm">
-                        {product?.description}
+                          {Array.from(
+                            { length: 5 - Math.floor(product.rating.rate) },
+                            (_, i) => (
+                              <StarIconOutline
+                                key={i}
+                                className="h-4 w-4 text-yellow-500"
+                              />
+                            )
+                          )}
+                        </div>
+                      )}
+                      <p className="text-blue-600 hover:underline cursor-pointer text-xs">
+                        See all {product?.rating.count} reviews
                       </p>
                     </div>
 
-                    <div className="space-y-3 text-sm">
-                      <button className="button w-full bg-blue-600 text-white border-transparent hover:border-blue-600 hover:bg-transparent hover:text-black">
-                        Add to bag
-                      </button>
-                      <button
-                        onClick={() => {
-                          window.location.reload();
-                        }}
-                        className="button w-full bg-transparent border-blue-600 hover:bg-blue-600 hover:text-white hover:border-transparent"
-                      >
-                        View full details
-                      </button>
-                    </div>
+                    <p className="line-clamp-5 text-sm">
+                      {product?.description}
+                    </p>
+                  </div>
+
+                  <div className="space-y-3 text-sm">
+                    <button className="button w-full bg-blue-600 text-white border-transparent hover:border-blue-600 hover:bg-transparent hover:text-black">
+                      Add to bag
+                    </button>
+                    <button
+                      onClick={() => window.location.reload()}
+                      className="button w-full bg-transparent border-blue-600 hover:bg-blue-600 hover:text-white hover:border-transparent"
+                    >
+                      View full details
+                    </button>
                   </div>
                 </div>
-              )}
-            </Dialog.Panel>
-          </div>
+              </div>
+            )}
+          </Dialog.Panel>
         </div>
       </div>
     </Dialog>
