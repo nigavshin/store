99754a8c18ea3e04ece2abf2d7e5056f1458a901
Nikita Gavshin <nikita.gavshin@piano.io>
2023-10-17 11:20:12 +0400

    feat: products and routes

---
+++ app/product/[id]/not-found.tsx
@@ -0,0 +1,10 @@
+function notFound() {
+  return (
+    <div className="mt-24">
+      <h2>Not found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  );
+}
+
+export default notFound;
